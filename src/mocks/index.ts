5cabf2c0cbefaf802e6c745e9c1f0781a3101ca3
matt-hulme <mattwhulme@gmail.com>
2024-04-27 20:15:57 -0600

    release 0.2

--- src/mocks/index.ts
+++
@@ -1,4 +0,0 @@
-export { classNamesMock } from './classNamesMock';
-export { jsxMock } from './jsxMock';
-export { jsxMock2 } from './jsxMock2';
-export { JsxMock3 } from './JsxMock3';
--- src/mocks/jsxMock2.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-export const jsxMock2: React.ReactElement = (
-    <div>
-      <div className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"></div>
-    </div>
-);
