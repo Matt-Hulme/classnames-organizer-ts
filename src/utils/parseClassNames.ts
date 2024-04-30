58d338a559f17056e89ae5a414018f552414f179
Matt-Hulme <Mattwhulme@gmail.com>
2024-04-30 08:18:09 -0600

    v-0.1.7-simplify-parsing

--- src/utils/parseClassNames.ts
+++ src/utils/parseClassNames.ts
@@ -1,11 +1,12 @@
 import * as parser from '@babel/parser';
 import traverse from '@babel/traverse';
-import generate from '@babel/generator';
 import { sortClassNames } from './sortClassNames';
 import { Identifier } from '@babel/types';
 
 export const parseClassNames = (tsxCode: string): string => {
   const ast = parser.parse(tsxCode, { sourceType: 'module', plugins: ['jsx', 'typescript', 'importMeta', 'dynamicImport'] });
+
+  const replacements = [];
 
   traverse(ast, {
     JSXAttribute(path) {
@@ -16,33 +17,53 @@ export const parseClassNames = (tsxCode: string): string => {
       if (path.node.value && path.node.value.type === 'StringLiteral') {
         const classNames = path.node.value.value;
         const sortedClassNames = sortClassNames(classNames);
-        path.node.value.value = sortedClassNames;
+        replacements.push({
+          start: path.node.value.start,
+          end: path.node.value.end,
+          value: `"${sortedClassNames}"`,
+        });
       } else if (path.node.value && path.node.value.type === 'JSXExpressionContainer') {
         const { expression } = path.node.value;
         if (expression.type === 'StringLiteral') {
           const classNames = expression.value;
           const sortedClassNames = sortClassNames(classNames);
-          expression.value = sortedClassNames;
+          replacements.push({
+            start: expression.start,
+            end: expression.end,
+            value: `"${sortedClassNames}"`,
+          });
         } else if (expression.type === 'CallExpression' && (expression.callee as Identifier).name === 'classNames') {
           expression.arguments.forEach(arg => {
             if (arg.type === 'StringLiteral') {
               const classNames = arg.value;
               const sortedClassNames = sortClassNames(classNames);
-              arg.value = sortedClassNames;
+              replacements.push({
+                start: arg.start,
+                end: arg.end,
+                value: `"${sortedClassNames}"`,
+              });
             }
           });
         } else if (expression.type === 'TemplateLiteral') {
           expression.quasis.forEach(quasi => {
             const classNames = quasi.value.raw;
             const sortedClassNames = sortClassNames(classNames);
-            quasi.value.raw = sortedClassNames;
-            quasi.value.cooked = sortedClassNames;
+            replacements.push({
+              start: quasi.start,
+              end: quasi.end,
+              value: `\`${sortedClassNames}\``,
+            });
           });
         }
       }
     },
   });
 
-  const { code } = generate(ast, { minified: false, retainLines: true });
-  return code;
+  // Apply replacements from the end of the code to the start
+  replacements.sort((a, b) => b.start - a.start);
+  for (const { start, end, value } of replacements) {
+    tsxCode = tsxCode.slice(0, start) + value + tsxCode.slice(end);
+  }
+
+  return tsxCode;
 };
