e7cf67849aa1a4a04ee69cae986ef8e93f411d7e
matt-hulme <mattwhulme@gmail.com>
2024-04-29 21:46:05 -0600

    ternaries and classNames (imports still broken)

--- src/utils/parseClassNames.ts
+++ src/utils/parseClassNames.ts
@@ -7,10 +7,37 @@ export const parseClassNames = (tsxCode: string): string => {
   const ast = parser.parse(tsxCode, { sourceType: 'module', plugins: ['jsx', 'typescript', 'importMeta', 'dynamicImport'] });
 
   traverse(ast, {
+    // Handle string literals
     StringLiteral(path) {
       const classNames = path.node.value;
       const sortedClassNames = sortClassNames(classNames);
       path.node.value = sortedClassNames;
+    },
+    // Handle classNames package usage
+    CallExpression(path) {
+      if (path.node.callee.name === 'classNames') {
+        path.node.arguments.forEach(arg => {
+          if (arg.type === 'StringLiteral') {
+            const classNames = arg.value;
+            const sortedClassNames = sortClassNames(classNames);
+            arg.value = sortedClassNames;
+          }
+        });
+      }
+    },
+    // Handle ternaries
+    ConditionalExpression(path) {
+      ['consequent', 'alternate'].forEach(key => {
+        const branch = path.node[key];
+        if (branch.type === 'TemplateLiteral') {
+          branch.quasis.forEach(quasi => {
+            const classNames = quasi.value.raw;
+            const sortedClassNames = sortClassNames(classNames);
+            quasi.value.raw = sortedClassNames;
+            quasi.value.cooked = sortedClassNames;
+          });
+        }
+      });
     },
   });
 
