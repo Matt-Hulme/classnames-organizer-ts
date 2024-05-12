29f6d1ae17f07fa9daf16f68b28b5e5ee36486af
matt-hulme <mattwhulme@gmail.com>
2024-05-12 13:47:13 -0600

    extension.ts revert to v.0.1.7

--- src/extension.ts
+++ src/extension.ts
@@ -1,52 +1,36 @@
-import * as vscode from 'vscode';
-import { parseClassNames } from './utils';
+import * as vscode from "vscode";
+import { parseClassNames } from "./utils";
 
 export function activate(context: vscode.ExtensionContext) {
-    let disposable = vscode.commands.registerCommand('extension.sortClassNames', async () => {
-        // Get the active editor
-        let editor = vscode.window.activeTextEditor;
-        if (!editor) {
-            console.log('No active text editor');
-            return; // No open text editor
-        }
+  let disposable = vscode.commands.registerCommand(
+    "extension.sortClassNames",
+    () => {
+      // Get the active editor
+      let editor = vscode.window.activeTextEditor;
+      if (!editor) {
+        console.log("No active text editor");
+        return; // No open text editor
+      }
 
-        // Get the document
-        let document = editor.document;
+      // Get the document
+      let document = editor.document;
 
-        // Get the text content of the document
-        let textContent = document.getText();
+      // Get the text content of the document
+      let textContent = document.getText();
 
-        // Call parseClassNames
-        const sortedCode = parseClassNames(textContent);
-        
-        // Replace the original code with the sorted code
-        await editor.edit(editBuilder => {
-            let fullRange = new vscode.Range(
-                document.positionAt(0),
-                document.positionAt(textContent.length)
-            );
-            editBuilder.replace(fullRange, sortedCode);
-        });
-    });
+      // Call parseClassNames
+      const sortedCode = parseClassNames(textContent);
 
-    context.subscriptions.push(disposable);
-
-    vscode.workspace.onWillSaveTextDocument(async event => {
-        // Get the document
-        let document = event.document;
-    
-        // Get the text content of the document
-        let textContent = document.getText();
-    
-        // Call parseClassNames
-        const sortedCode = parseClassNames(textContent);
-    
-        // Replace the original code with the sorted code
-        const edit = new vscode.WorkspaceEdit().replace(
-            document.uri, 
-            new vscode.Range(document.positionAt(0), document.positionAt(textContent.length)), 
-            sortedCode
+      // Replace the original code with the sorted code
+      editor.edit((editBuilder) => {
+        let fullRange = new vscode.Range(
+          document.positionAt(0),
+          document.positionAt(textContent.length)
         );
-        const success = await vscode.workspace.applyEdit(edit);
-g    });
+        editBuilder.replace(fullRange, sortedCode);
+      });
+    }
+  );
+
+  context.subscriptions.push(disposable);
 }
